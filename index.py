37442212662b3a88be06d30abf2ccd475d62170d
lumumbar <robert21lumumba@gmail.com>
2024-12-12 18:00:58 +0300

    Python week 1

---
+++ index.py
@@ -0,0 +1,20 @@
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operator = input("Enter the operation (+, -, *, /): ")
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            print("Error: Division by zero")
+            return
+        result = num1 / num2
+    else:
+        print("Invalid operator")
+        return
+
+    print("Result:", result)
